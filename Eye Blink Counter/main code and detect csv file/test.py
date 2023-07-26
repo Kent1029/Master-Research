7c687b3cf43757fb3fc586adc4fa71a49dbfe04a
Kent1029 <y787896@gmail.com>
2023-07-26 14:06:26 +0800

    update

--- Eye Blink Counter/main code and detect csv file/test.py
+++ Eye Blink Counter/main code and detect csv file/test.py
@@ -7,3 +7,5 @@ avg_blink_duration = data['avg_blink_duration'].values
 print("blink_count::", blink_count)
 print("type(blink_count)",type(blink_count))
 #print("avg_blink_duration::", avg_blink_duration)
+print("blink_count.shape:::",blink_count.shape)
+print("avg_blink_duration.shape:::",avg_blink_duration.shape)
