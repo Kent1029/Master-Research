5ec109994e149828642b19c68099363606bc8845
Kent1029 <y787896@gmail.com>
2023-11-01 16:10:15 +0800

    update

--- Emotion-detection/main_code_csv/Emotion_change_detection.py
+++ Emotion-detection/main_code_csv/Emotion_change_detection.py
@@ -9,6 +9,21 @@ from tqdm import tqdm
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+
 
 # Constants and configurations
 #FOLDER_PATH ='E:\\Research\\dataset\\FaceForensics++\\original_sequences\\youtube\\c23\\videos'
